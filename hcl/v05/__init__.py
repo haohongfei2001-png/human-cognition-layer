65d8d97f9c2cb77797dddb6ba6f1c9e9ab946710
hongfei hao <haohongfei2001@gmail.com>
2026-09-24 18:58:27 +0800

    v05: export semantic runtime integration

--- hcl/v05/__init__.py
+++ hcl/v05/__init__.py
@@ -17,3 +17,20 @@ __all__ = [
     "project_all_current_stances",
     "project_current_stance",
 ]
+
+from .runtime import HCLV05Runtime, V05IngestResult
+from .semantic import (
+    ExtractionResult,
+    SemanticExtractionError,
+    catalog_from_events,
+    extract_stance_events,
+)
+
+__all__ += [
+    "ExtractionResult",
+    "HCLV05Runtime",
+    "SemanticExtractionError",
+    "V05IngestResult",
+    "catalog_from_events",
+    "extract_stance_events",
+]
